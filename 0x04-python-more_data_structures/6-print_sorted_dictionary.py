4f119ca7e635637278fbdb5bbf942642d2c0776d
Beldine-Moturi <beldinemoturi@gmail.com>
2021-11-29 23:57:36 +0300

    pep8 validation

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def  print_sorted_dictionary(a_dictionary):
+def print_sorted_dictionary(a_dictionary):
     """print a dictionary by ordered keys"""
     my_list = list(a_dictionary.keys())
     for key in sorted(my_list):
