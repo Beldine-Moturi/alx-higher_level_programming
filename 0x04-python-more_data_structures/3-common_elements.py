98432d9280c58454442283458c3b750162ef3cd2
Beldine-Moturi <beldinemoturi@gmail.com>
2021-11-29 23:29:25 +0300

    return list

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
     """return set of commom elements in set1 and set 2"""
-    return set_1.intersection(set_b)
+    return list(set_1.intersection(set_b))
