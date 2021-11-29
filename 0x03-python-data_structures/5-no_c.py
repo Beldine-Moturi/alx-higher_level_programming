8294a1620472b3d651132e0d8d9cabff84862213
Beldine-Moturi <beldinemoturi@gmail.com>
2021-11-29 15:03:03 +0300

    use join method

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
     """remove characters from a string"""
-    new_string = ""
-    for char in my_string:
-        if char == 'c' or char == 'C':
-            continue
-        else:
-            new_string += char
-    return new_string
+    myList = [char for char in my_string if char != 'c' or char != 'C']
+    return ("".join(myList))
