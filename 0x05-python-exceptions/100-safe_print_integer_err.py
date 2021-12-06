f263a4ef0642267fd21a0c89b005d22e92858a21
Beldine-Moturi <beldinemoturi@gmail.com>
2021-12-07 01:32:23 +0300

    print to stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
     """prints error messages to stderr"""
+    import sys
     try:
         print("{:d}".format(value))
     except Exception as ex:
-        print("Exception: ", ex)
+        print("Exception: ", ex, file=sys.stderr)
         return False
     else:
         return True
