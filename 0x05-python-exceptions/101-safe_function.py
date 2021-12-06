47c9ef3ef193b3beda9fdd3eecb6f982c23bac47
Beldine-Moturi <beldinemoturi@gmail.com>
2021-12-07 01:46:34 +0300

    print to stderr

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+def safe_function(fct, *args):
+    """excecutes fct safely"""
+    try:
+        result = fct(*args)
+    except Exception as ex:
+        print("Exception: {}".format(ex), file=sys.stderr)
+        return None
+    else:
+        return result
