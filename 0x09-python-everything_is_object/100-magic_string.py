7d06d43a38fc93c4209e4162a2813120da574033
Beldine-Moturi <beldinemoturi@gmail.com>
2021-12-15 01:15:56 +0300

    remove broken code

--- 0x09-python-everything_is_object/100-magic_string.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-def magic_string():
-    if not hasattr(magic_string, 'count'): magic_string.count = 0
-    magic_string.count += 1
-    return ('Best' + ', Best' * (magic_string.count - 1))
