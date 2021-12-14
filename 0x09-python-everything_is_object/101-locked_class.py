cddbefefdc96173be736724e6964833119138b3d
Beldine-Moturi <beldinemoturi@gmail.com>
2021-12-14 17:37:17 +0300

    advanced tasks

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module defines a class 
+that prevents users from dynamically creating new attributes
+"""
+
+
+class LockedClass:
+    """
+    class that allows only one attribute creation
+    """
+    __slot__ = 'first_name'
+    def __init__(self, name=''):
+        """
+        Initializes new objects
+        """
+        self.first_name = name
