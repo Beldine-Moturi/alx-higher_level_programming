66dd78f7ac486e6c2dbdef4877df6c3d74cf4270
Beldine-Moturi <beldinemoturi@gmail.com>
2021-12-14 17:40:44 +0300

    pep8 validation

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This module defines a class 
+This module defines a class
 that prevents users from dynamically creating new attributes
 """
 
@@ -10,6 +10,7 @@ class LockedClass:
     class that allows only one attribute creation
     """
     __slot__ = 'first_name'
+
     def __init__(self, name=''):
         """
         Initializes new objects
