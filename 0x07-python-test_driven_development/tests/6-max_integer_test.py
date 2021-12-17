54dcc8ce38114f0fdf9da7023695e6dbdac7110a
Beldine-Moturi <beldinemoturi@gmail.com>
2021-12-17 14:48:04 +0300

    corrections

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -11,31 +11,38 @@ class TestMaxInteger(unittest.TestCase):
     """
     This class provides methods to test the max_integer function
     """
-    def test_integer_list(self):
+    def test_max_end(self):
         """
-        This method tests correct output when a list of integers is provided
+        Tests max at the end
         """
-        self.assertAlmostEqual(max_integer([1, 3, 4, 2]), 4)
+        self.assertAlmostEqual(max_integer([1, 3, 2, 4]), 4)
+    def test_max_beginning(self):
+        """
+        tests max at the beginning
+        """
         self.assertAlmostEqual(max_integer([10, 8, 6, 4]), 10)
-        self.assertAlmostEqual(max_integer([34, 32, 33, 38]), 38)
+    def test_max_middle(self):
+        """
+        tests max in the middle
+        """
+        self.assertAlmostEqual(max_integer([34, 32, 39, 38]), 39)
+    def test_none(self):
+        """
+        tests none argument
+        """
         self.assertAlmostEqual(max_integer([]), None)
+    def test_one_arg(self):
+        """
+        tests one argument in list
+        """
         self.assertAlmostEqual(max_integer([32]), 32)
+    def test_one_neg(self):
+        """
+        tests when one negative integer in list
+        """
         self.assertAlmostEqual(max_integer([10, 5, -80, 4]), 10)
+    def test_all_neg(self):
+        """
+        Tests when list has all negative numbers
+        """
         self.assertAlmostEqual(max_integer([-8, -4, -3, -10]), -3)
-
-    def test_type(self):
-        """
-        This method tests for correct type(list of integers) given as the
-        function argument
-        """
-        self.assertRaises(TypeError, max_integer, 'string')
-        self.assertRaises(TypeError, max_integer, True)
-        self.assertRaises(TypeError, max_integer, 3+5j)
-
-    def test_list_values(self):
-        """
-        This method tests for correct values(integers or floats only) given in
-        the list argument
-        """
-        self.assertRaises(TypeError, max_integer, [1, 2, 'string', 4])
-        self.assertRaises(TypeError, max_integer, [3, True, 6, 8])
