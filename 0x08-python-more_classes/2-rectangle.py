2e58082b193de6562607caf5d127e1f53f8a77bf
Beldine-Moturi <beldinemoturi@gmail.com>
2021-12-21 16:29:09 +0300

    tests

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -56,7 +56,7 @@ class Rectangle:
     def area(self):
         """returns the area of the rectangle"""
 
-        return (self.__width * self.__height)
+        return (self.width * self.height)
 
 
     def perimeter(self):
