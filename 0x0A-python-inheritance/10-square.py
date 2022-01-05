4f34f140ad44c7f3e5d5d33b8623ee9db689008f
Beldine-Moturi <beldinemoturi@gmail.com>
2022-01-05 13:48:06 +0300

    multiple inheritance

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
-"""Defines a Rectangle subclass Square."""
+"""Defines a class Square that inherits from class Rectangle"""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Represent a square."""
+    """Description for the class Square"""
 
     def __init__(self, size):
-        """Initialize a new square.
+        """Initializes instances of the class"""
 
-        Args:
-            size (int): The size of the new square.
-        """
-        self.integer_validator("size", size)
-        super().__init__(size, size)
+        super().integer_validator('size', size)
         self.__size = size
+
+    def area(self):
+        """Returns the area of the square"""
+
+        return (self.__size ** 2)
+
+    def __str__(self):
+        """Returns a string representation of the Square"""
+
+        return ("[Rectangle] {}/{}".format(self.__size, self.__size))
