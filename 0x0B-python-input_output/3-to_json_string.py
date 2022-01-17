b51546b6f59b76f9078fe89e0470883560bfc16f
Beldine-Moturi <beldinemoturi@gmail.com>
2022-01-17 07:47:41 +0300

    serialize python obj to json

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""Defines a string-to-JSON function."""
+"""Defines a function ``to_json_string()``"""
 import json
 
 
 def to_json_string(my_obj):
-    """Return the JSON representation of a string object."""
+    """returns the JSON representation of an object my_obj (string)"""
+
     return json.dumps(my_obj)
