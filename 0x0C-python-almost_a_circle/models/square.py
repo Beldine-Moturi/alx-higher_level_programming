de8cedc20057de6f5cece5f4160b18f1c051dc77
Beldine-Moturi <beldinemoturi@gmail.com>
2022-02-01 15:32:15 +0300

    write Square class to pass basic tests

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Defines a class Square that inherits from Rectangle"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Description for instances of the class Square"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initializes instances of the Square
+
+        Args:
+            size (int): the size of the square instance
+            x (int): x coordinate used to print out the square
+            y (int): y coordinate used to print out the square
+            id (int): identifier for a square instance
+        """
+
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Returns a string representation for Square instances"""
+
+        return f"[{self.__class__.__name__}] ({self.id}) {self.x}/{self.y}\
+ - {self.width}"
