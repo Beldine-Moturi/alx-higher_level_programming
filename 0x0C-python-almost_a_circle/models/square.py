0cc5fad370edd547e97c850ca26770f9956befe2
Beldine-Moturi <beldinemoturi@gmail.com>
2022-02-01 19:00:26 +0300

    update Square class with to_dictionary method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -69,4 +69,9 @@ class Square(Rectangle):
 
     def to_dictionary(self):
         """Returns a dictionary representation of a Rectangle instance"""
-        pass
+        return {
+            'size': self.size,
+            'x': self.x,
+            'y': self.y,
+            'id': self.id
+        }
