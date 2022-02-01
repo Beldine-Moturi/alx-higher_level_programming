52bdcad055efb50155916eb8f1d73d2911c55507
Beldine-Moturi <beldinemoturi@gmail.com>
2022-02-01 16:07:40 +0300

    update Square class with size getter and setter

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,6 +18,17 @@ class Square(Rectangle):
 
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """returns the size of the square"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Sets the value for the size of the square"""
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """Returns a string representation for Square instances"""
 
