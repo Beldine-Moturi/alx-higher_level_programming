4f970b8b9023d2ead87933230dc2b56f5cd433ab
Beldine-Moturi <beldinemoturi@gmail.com>
2022-03-01 03:09:03 +0300

    [fix]: fix indentation

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const myVar = Number(process.argv[2]);
 if (!(Number.isInteger(myVar))) {
-    console.log('Missing size');
+  console.log('Missing size');
 } else {
-    for (let i = 0; i < myVar; i++) {
-	for (let n = 0; n < myVar; n++) {
-	    process.stdout.write('x');
-	}
-	process.stdout.write('\n');
+  for (let i = 0; i < myVar; i++) {
+    for (let n = 0; n < myVar; n++) {
+      process.stdout.write('x');
     }
+    process.stdout.write('\n');
+  }
 }
