6ca2b2a0d956eae4b1e06aca888d881cf10f4fa8
Beldine-Moturi <beldinemoturi@gmail.com>
2022-03-01 03:21:03 +0300

    [fix]: fix indentation

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,9 +3,9 @@ const myVar = Number(process.argv[2]);
 if (!(Number.isInteger(myVar))) {
   console.log('Missing size');
 } else {
-    for (let i = 0; i < myVar; i++) {
-	let row = '';
-	for (let n = 0; n < myVar; n++) row += 'X';
-	console.log(row);
-    }
+  for (let i = 0; i < myVar; i++) {
+    let row = '';
+    for (let n = 0; n < myVar; n++) row += 'X';
+    console.log(row);
+  }
 }
