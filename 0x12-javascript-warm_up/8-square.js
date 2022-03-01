608bea93fefdc9775cd10957dea263e179d6561e
Beldine-Moturi <beldinemoturi@gmail.com>
2022-03-01 03:17:30 +0300

    [refactor]: use console.lo to print

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,10 +3,9 @@ const myVar = Number(process.argv[2]);
 if (!(Number.isInteger(myVar))) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < myVar; i++) {
-    for (let n = 0; n < myVar; n++) {
-      process.stdout.write('x');
+    for (let i = 0; i < myVar; i++) {
+	let row = '';
+	for (let n = 0; n < myVar; n++) row += 'X';
+	console.log(row);
     }
-    process.stdout.write('\n');
-  }
 }
