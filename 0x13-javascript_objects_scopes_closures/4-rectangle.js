61eb98b6fd8f47c5925eec935e21fcb837687436
Beldine-Moturi <beldinemoturi@gmail.com>
2022-03-03 01:59:06 +0300

    [feat]: add two more instance methods

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let char = '';
+      for (let n = 0; n < this.width; n++) char += 'X';
+      console.log(char);
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
