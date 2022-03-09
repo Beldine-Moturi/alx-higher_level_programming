c2400ff71d41d5389e26532d2199e45c6848c025
Beldine-Moturi <beldinemoturi@gmail.com>
2022-03-10 01:47:12 +0300

    fix bug

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -18,9 +18,8 @@ if __name__ == "__main__":
 
     cur = db.cursor()
     cur.execute(
-        "SELECT * FROM states\
-            WHERE name LIKE BINARY %s\
-            ORDER BY id ASC;", (db_info[3]), )
+        "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC;",
+        (db_info[3], ))
 
     rows = cur.fetchall()
     for row in rows:
