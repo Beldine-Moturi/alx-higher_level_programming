198e5a706b9b28c7e3bb782bc99bfe6fdc0c552c
Beldine-Moturi <beldinemoturi@gmail.com>
2022-03-10 01:02:00 +0300

    filter state names that match a certain string

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""select all states with name matching a certain string"""
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    db_info = sys.argv[1:]
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=db_info[0],
+        passwd=db_info[1],
+        db=db_info[2],
+        charset='utf8'
+    )
+
+    cur = db.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name = %s ORDER BY id ASC;", (db_info[3],)
+    )
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
