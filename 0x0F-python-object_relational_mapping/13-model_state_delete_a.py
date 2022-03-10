410cebdf983171f0652da8daec1be03a82ee9124
Beldine-Moturi <beldinemoturi@gmail.com>
2022-03-11 01:22:09 +0300

    delete rows from table

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""update the name of a row in the table"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+import sys
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3]
+    ), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).filter(State.name.like("%a%"))\
+                                 .delete(synchronize_session='fetch')
+    session.commit()
+
+    session.close()
