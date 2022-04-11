3969f67a3beee4851c615c3ded9b45d98e614200
Beldine-Moturi <beldinemoturi@gmail.com>
2022-04-12 00:33:25 +0300

    display all HTTTP methods server will accept

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL and displays all HTTP methods the server will accept.
-curl -sIX OPTIONS "$1" | grep -i Allow | cut -d ":" -f 2 | cut 2-
+curl -sIX OPTIONS "$1" | grep -i Allow | cut -d ":" -f 2 | cut -b 2-
