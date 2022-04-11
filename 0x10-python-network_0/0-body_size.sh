69ca4bc409a05ec15b0d231f2a41a4ccfe7a12e9
Beldine-Moturi <beldinemoturi@gmail.com>
2022-04-11 23:41:11 +0300

    display body size in HTTP response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # sends a request to a URL(command line argument), and displays the size of the body of the response
 
 curl -sI "$1" | grep -i Content-Length | cut -d " " -f 2
