dded2c793db11079b4b62a399716ca07df058027
Beldine-Moturi <beldinemoturi@gmail.com>
2022-04-12 16:28:19 +0300

    post data to url

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+sends a POST request to the passed URL with the email as a parameter
+and finally displays the body of the response.
+"""
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    url = sys.argv[1]
+    values = {'email': sys.argv[2]}
+
+    r = requests.post(url, data=values)
+    print(r.text)
