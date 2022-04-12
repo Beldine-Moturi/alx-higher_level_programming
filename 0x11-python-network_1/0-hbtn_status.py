e2955d818b3706c725c2f4f3d9e5f9595b6cdc5b
Beldine-Moturi <beldinemoturi@gmail.com>
2022-04-12 14:28:10 +0300

    pep8 validation

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     import urllib.request
 
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as\
-          response:
+         response:
         res = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(res)))
