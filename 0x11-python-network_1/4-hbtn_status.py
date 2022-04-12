9d6364ebc3da4741289cd8de242250281ae05f12
Beldine-Moturi <beldinemoturi@gmail.com>
2022-04-12 16:07:55 +0300

    fetch url using requests package

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+ fetches https://alx-intranet.hbtn.io/status
+Using python's requests package
+"""
+if __name__ == "__main__":
+    import requests
+
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print(f"\t- type: {type(r.text)}")
+    print(f"\t- content: {r.text}")
