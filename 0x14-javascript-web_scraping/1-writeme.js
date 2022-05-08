a4944da7b15ea327ecc6d8a0b0f3bb9fd4f9b0a1
Beldine-Moturi <beldinemoturi@gmail.com>
2022-05-08 23:42:27 +0300

    fix indentations

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,5 +5,5 @@ const filePath = process.argv[2];
 const text = process.argv[3];
 
 fs.writeFile(filePath, text, 'utf-8', (err) => {
-    if (err) { console.log(err) };
+  if (err) { console.log(err); }
 });
