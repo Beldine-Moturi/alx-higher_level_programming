ed9cf453355e704899acee26ad95bd523ea156e4
Beldine-Moturi <beldinemoturi@gmail.com>
2022-05-09 02:45:55 +0300

    fetch star wars API

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    let count = 0;
+    const movies = JSON.parse(body).results;
+    for (const movie of movies) {
+	    const movieChars = movie.characters;
+	    for (const movieChar of movieChars) {
+        if (movieChar.includes('18')) {
+		    count++;
+        }
+	    }
+    }
+    console.log(count);
+  } else {
+    console.log(`Error code: ${response.statusCode}`);
+  }
+});
