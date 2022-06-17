5e380f024da0333c31c107bff622329c618d07f1
Beldine-Moturi <beldinemoturi@gmail.com>
2022-06-17 18:13:36 +0300

    fix erros

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,27 +1,35 @@
 #!/usr/bin/node
 
-const request = require('request');
-const id = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${id}`;
+const argv = process.argv;
+const urlFilm = 'https://swapi-api.hbtn.io/api/films/';
+const urlMovie = `${urlFilm}${argv[2]}/`;
 
-request.get(url, (err, response, body) => {
-  if (err) {
-      console.log('1:', err);
-  } else if (response.statusCode === 200) {
-    const characters = JSON.parse(body).characters;
-    for (let i in characters) {
-      request.get(characters[i], (err, response, body) => {
-        if (err) {
-          console.log(err);
-        } else if (response.statusCode === 200) {
-          const name = JSON.parse(body).name;
-          console.log(name);
-        } else {
-          console.log(`Error code: ${response.statusCode}`);
-        }
-      });
+const request = require('request');
+
+request(urlMovie, function (error, response, body) {
+  if (error == null) {
+    const fbody = JSON.parse(body);
+    const characters = fbody.characters;
+
+    if (characters && characters.length > 0) {
+      const limit = characters.length;
+      CharRequest(0, characters[0], characters, limit);
     }
   } else {
-    console.log(`Error code 1: ${response.statusCode}`);
+    console.log(error);
   }
 });
+
+function CharRequest (idx, url, characters, limit) {
+  if (idx === limit) { return; }
+  request(url, function (error, response, body) {
+    if (!error) {
+      const rbody = JSON.parse(body);
+      console.log(rbody.name);
+      idx++;
+      CharRequest(idx, characters[idx], characters, limit);
+    } else {
+      console.error('error:', error);
+    }
+  });
+}
